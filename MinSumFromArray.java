26e66ee44248b0fb69d964752ef11f3baadb29f7
Nimit3-droid <Nimit3-droid@users.noreply.github.com>
2021-06-13 12:49:56 +0530

    MinsunFromarray

---
+++ MinSumFromArray.java
@@ -0,0 +1,23 @@
+import java.util.*;;
+public class MinSumFromArray extends sumOfTwoLargeNumbers {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        String odd="";
+        String even="";
+        for(int i=0;i<n;i+=2) {
+            even+=Integer.toString(arr[i]);
+            if(i+1<n){
+                odd+=Integer.toString(arr[i+1]);
+            }
+        }
+        System.out.println(FindBigSum(even,odd));
+    }
+}
--- sumOfTwoLargeNumbers.java
+++ sumOfTwoLargeNumbers.java
@@ -1,3 +1,47 @@
+import java.util.Scanner;
+
 public class sumOfTwoLargeNumbers {
-    
+
+    public static String FindBigSum(String s1, String s2) {
+        int n1 = s1.length(), n2 = s2.length();
+        if (n2 > n1) {
+            String temp = s1;
+            s1 = s2;
+            s2 = temp;
+            n1 = s1.length();
+            n2 = s2.length();
+        }
+        // int diff = n1 - n2;
+        int carry = 0;
+        int i = n1 - 1, j = n2 - 1;
+        String answer = "";
+        while (j >= 0 && i >= 0) {
+            int sum = Integer.parseInt(s1.charAt(i) + "") + Integer.parseInt(s2.charAt(j) + "") + carry;
+            answer = Integer.toString(sum % 10) + answer;
+            carry = sum / 10;
+            // diff--;
+            i--;
+            j--;
+        }
+
+        for (int leftOver = n1 - n2 - 1; leftOver >= 0; leftOver--) {
+            int sum = Integer.parseInt(s1.charAt(leftOver) + "") + carry;
+            answer = Integer.toString(sum % 10) + answer;
+            carry = sum / 10;
+        }
+
+        if (carry > 0) {
+            answer = Integer.toString(carry) + answer;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next(),str2 = sc.next();
+        // String str1 = "1234", str2 = "456";
+        System.out.println(FindBigSum(str1, str2));
+
+    }
 }
